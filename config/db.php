f61dc95a9fb721e340658d704da24965d6d3a329
rodrigoigawa <igawa.rodrigo@gmail.com>
2016-06-13 08:23:41 -0300

    add config db to gitignore

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=firstApp',
     'username' => 'root',
-    'password' => '',
+    'password' => 'uniware2016',
     'charset' => 'utf8',
 ];
