658b0d751de2717f4b77548e6be59700a0d3eb19
rodrigoigawa <igawa.rodrigo@gmail.com>
2016-06-13 08:43:05 -0300

    Remove ignored files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=firstApp',
-    'username' => 'root',
-    'password' => 'uniware2016',
-    'charset' => 'utf8',
-];
